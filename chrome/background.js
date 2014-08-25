b6b8b94b499ca1db48cbf3ff8b01416d0bea893f
Lindy Roquemore <lindy@oddmouse.com>
2014-08-25 15:52:55 -0500

    Update packaged chrome app
    - point webview directly to website
    - allow cache to remain after browser close

--- chrome/background.js
+++ chrome/background.js
@@ -1,6 +1,7 @@
 chrome.app.runtime.onLaunched.addListener(function() {
   chrome.app.window.create('window.html', {
     resizable: false,
+    id: "partymode",
     bounds: {
       width: 320,
       height: 480
